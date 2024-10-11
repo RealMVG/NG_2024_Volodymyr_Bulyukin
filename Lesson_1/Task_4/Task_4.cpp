6c710cef9bb226047adb6fa6972b21bb15cc89d0
Kuroshi23 <46668940+RealMVG@users.noreply.github.com>
2024-10-11 22:13:05 +0300

    Added Task_4

--- Lesson_1/Task_4/Task_4.cpp
+++ Lesson_1/Task_4/Task_4.cpp
@@ -4,4 +4,35 @@ using namespace std;
 
 int main (){
 
+    int salary;
+
+    cout << "Write your salary, sir:" << endl;
+    cin >> salary;
+
+    int salaryCategory = 0;
+
+
+    if (salary < 999) {
+        salaryCategory = 1;
+    }
+    if (salary > 999) {
+        if (salary < 999999) {
+            salaryCategory = 2;
+        }
+    }
+    if (salary > 999999) {
+        salaryCategory = 3;
+    }
+
+    switch (salaryCategory) {
+    case 1:
+        cout << "Work harder!!!" << endl;
+        break;
+    case 2:
+        cout << "Good job!" << endl;
+        break;
+    case 3:
+        cout << "OMG! U Millionare!" << endl;
+        break;
+    }
 }
