600a892b612f624d4e43959ac7f1f1fff8365234
Kuroshi23 <46668940+RealMVG@users.noreply.github.com>
2024-10-16 18:37:23 +0300

    Changed Task_3.cpp

--- Lesson_1/Task_3/Task_3.cpp
+++ Lesson_1/Task_3/Task_3.cpp
@@ -3,27 +3,32 @@
 using namespace std;
 
 int main (){
-    double a, b;
-    char operation;
+    double a, b; char operation; int result; bool correct = true;
 
     cout << "Calculator :3" << endl;
 
     cout << "Write what u want to do in format like: 1+1;\nSupported chars (+, -, *, /)" << endl;
-    cin >> a; cin >> operation; cin >> b;
+    cin >> a >> operation >> b;
 
     switch(operation){
-        case '+':
-            cout << "Result: " << (a+b) << endl;
-            break;
-        case '-':
-            cout << "Result: " << (a-b) << endl;
-            break;
-        case '*':
-           cout << "Result: " << (a*b) << endl;
-            break;
-        case '/':
-           cout << "Result: " << (a/b) << endl;
-            break;
+    case '+':
+        result = a + b;
+        break;
+    case '-':
+        result = a - b;
+        break;
+    case '*':
+        result = a * b;
+        break;
+    case '/':
+        result = a / b;
+        break;
+    default:
+        correct = false;
+        cout << "wrong symbol" << endl;
+        break;
     }
-        return 0;
+    if (correct == true){
+        cout << result << endl;
+    }
 }
