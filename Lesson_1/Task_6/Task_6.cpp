d20fd08fb055086ec6c33a58fe804ae090e170ff
Kuroshi23 <46668940+RealMVG@users.noreply.github.com>
2024-10-20 00:58:29 +0300

    Task_6 added signed check and error types

--- Lesson_1/Task_6/Task_6.cpp
+++ Lesson_1/Task_6/Task_6.cpp
@@ -9,64 +9,71 @@ int main() {
     short height, anim = 1, sleep, Lstart = 0, Lcount;
 
     cout << "Tringle Animator\nType Tringle height: ", cin >> height;
-    cout << "Type Speed of animation (ms): ", cin >> sleep, cout << "Type how much times animation will play: ", cin >> Lcount;
+    if (height > 0){
+        cout << "Type Speed of animation (ms): ", cin >> sleep;
+        if (sleep > 0){
+            cout << "Type how much times animation will play: ", cin >> Lcount;
+            if (Lcount > 0){
+                system("cls");
+                do
+                    switch(anim) {
+                    case 1:
+                        for (int i = 0; i < height; i++) {
+                            for (int j = 0; j < i + 1; j++) {
+                                cout << "*";
+                            }
 
-    if (height > 0 && Lcount > 0 && sleep > 0) {
-        system("cls");
-        do
-            switch(anim) {
-            case 1:
-                for (int i = 0; i < height; i++) {
-                    for (int j = 0; j < i + 1; j++) {
-                        cout << "*";
+                            cout << endl;
+                        }
+                        Sleep(sleep); system("cls");
+                    case 2:
+                        for (int i = height; i > 0; i--) {
+                            for (int j = 0; j < i; j++) {
+                                cout << "*";
+                            }
+
+                            cout << endl;
+                        }
+                        Sleep(sleep); system("cls");
+                    case 3:
+                        for (int i = height; i > 0; i--) {
+
+                            for (int j = 0; j < height - i; j++) {
+                                cout << ' ';
+                            }
+
+                            for (int j = 0; j < i; j++) {
+                                cout << "*";
+                            }
+
+                            cout << endl;
+                        }
+                        Sleep(sleep); system("cls");
+                    case 4:
+                        for (int i = 0; i < height; i++) {
+
+                            for (int j = 0; j < height - i - 1; j++) {
+                                cout << ' ';
+                            }
+
+                            for (int j = 0; j < 1 * i + 1; j++) {
+                                cout << "*";
+                            }
+
+                            cout << endl;
+                        }
+                        Lstart++;
+                        Sleep(sleep); system("cls");
                     }
-
-                    cout << endl;
-                }
-                Sleep(sleep); system("cls");
-            case 2:
-                for (int i = height; i > 0; i--) {
-                    for (int j = 0; j < i; j++) {
-                        cout << "*";
-                    }
-
-                    cout << endl;
-                }
-                Sleep(sleep); system("cls");
-            case 3:
-                for (int i = height; i > 0; i--) {
-
-                    for (int j = 0; j < height - i; j++) {
-                        cout << ' ';
-                    }
-
-                    for (int j = 0; j < i; j++) {
-                        cout << "*";
-                    }
-
-                    cout << endl;
-                }
-                Sleep(sleep); system("cls");
-            case 4:
-                for (int i = 0; i < height; i++) {
-
-                    for (int j = 0; j < height - i - 1; j++) {
-                        cout << ' ';
-                    }
-
-                    for (int j = 0; j < 1 * i + 1; j++) {
-                        cout << "*";
-                    }
-
-                    cout << endl;
-                }
-                Lstart++;
-                Sleep(sleep); system("cls");
+                while (Lstart < Lcount);
+                cout << "Animation loop ended success!!! <3\n";
+            } else {
+                cout << "Error 3: LoopCount < 0\n";
+            }
+        } else{
+            cout << "Error 2: (ms) < 0\n";
         }
-        while (Lstart < Lcount);
-        cout << "Animation loop ended success!!! <3\n";
-    }
-    else {
-        cout << "No, no, no. Without sighted numbers, pls! (Error # 1)" << endl;
+    } else {
+        cout << "Error 1: Height < 0\n";
     }
 }
