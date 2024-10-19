aaa52afbfee251adac8859316e9969024db74df2
Kuroshi23 <46668940+RealMVG@users.noreply.github.com>
2024-10-19 23:45:57 +0300

    Task_6 little bit reworked

--- Lesson_1/Task_6/Task_6.cpp
+++ Lesson_1/Task_6/Task_6.cpp
@@ -4,25 +4,19 @@
 using namespace std;
 
 int main() {
-    short height, anim = 1, sleep;
+    SetConsoleOutputCP(CP_UTF8); // Console Fix
 
-    cout << "Type Tringle height: ";
-    cin >> height;
-    cout << "Type Speed of animation (ms): ";
-    cin >> sleep;
+    short height, anim = 1, sleep, Lstart = 0, Lcount;
 
+    cout << "Tringle Animator\nType Tringle height: ", cin >> height;
+    cout << "Type Speed of animation (ms): ", cin >> sleep, cout << "Type how much times animation will play: ", cin >> Lcount;
 
-    if (height > 0) {
+    if (height > 0 && Lcount > 0 && sleep > 0) {
         system("cls");
         do
             switch(anim) {
             case 1:
                 for (int i = 0; i < height; i++) {
-
-                    // for (int j = 0; j < height - i - 1; j++) {
-                    //     cout << ' ';
-                    // }
-
                     for (int j = 0; j < i + 1; j++) {
                         cout << "*";
                     }
@@ -66,11 +60,13 @@ int main() {
 
                     cout << endl;
                 }
+                Lstart++;
                 Sleep(sleep); system("cls");
         }
-        while (true);
+        while (Lstart < Lcount);
+        cout << "Animation loop ended success!!! <3\n";
     }
     else {
-        cout << "No, no, no. Without sighted numbers, pls!" << endl;
+        cout << "No, no, no. Without sighted numbers, pls! (Error # 1)" << endl;
     }
 }
